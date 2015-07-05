3c8367a88549e1cdebb389288d05a9bdbcc424ff
Havi S <havi@blitterfly.com>
2015-07-05 16:16:05 -0700

    First commit

---
+++ src/halo-watchface.c
@@ -0,0 +1,193 @@
+#include <pebble.h>
+
+#define KEY_TWEET 0
+
+static Window * s_main_window;
+static TextLayer * s_time_layer;
+static TextLayer * s_tweet_layer;
+static GFont s_time_font;
+static GFont s_tweet_font;
+static BitmapLayer * s_background_layer;
+static GBitmap * s_background_bitmap;
+static BitmapLayer * s_twitter_icon_layer;
+static GBitmap * s_twitter_icon_bitmap;
+
+static void update_time() {
+	// Get a tm structure
+	time_t temp = time(NULL); 
+	struct tm * tick_time = localtime(&temp);
+	
+	// Create a long-lived buffer
+	static char buffer[] = "00:00";
+	
+	// Write the current hours and minutes into the buffer
+	if (clock_is_24h_style() == true) {
+		// Use 24 hour format
+		strftime(buffer, sizeof("00:00"), "%k:%M", tick_time);
+	} else {
+		// Use 12 hour format
+		strftime(buffer, sizeof("00:00"), "%l:%M", tick_time);
+	}
+	
+	// Display this time on the TextLayer
+	text_layer_set_text(s_time_layer, buffer[0] == ' ' ? (buffer + 1) : buffer);
+}
+
+static void tick_handler(struct tm * tick_time, TimeUnits units_changed) {
+	update_time();
+	
+	// Get latest tweet update every 30 minutes
+	if(tick_time->tm_min % 30 == 0) {
+		// Begin dictionary
+		DictionaryIterator * iter;
+		app_message_outbox_begin(&iter);
+		
+		// Add a key-value pair
+		dict_write_uint8(iter, 0, 0);
+		
+		// Send the message!
+		app_message_outbox_send();
+	}
+}
+
+static void main_window_load(Window * window) {
+	// Create GBitmap, then set to created BitmapLayer
+	s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+	s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+	
+	// Create time TextLayer
+	s_time_layer = text_layer_create(GRect(5, 0, 134, 45));
+	text_layer_set_background_color(s_time_layer, GColorClear);
+	text_layer_set_text_color(s_time_layer, GColorWhite);
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	
+	// Create Twitter icon layer
+	s_twitter_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TWITTER_ICON);
+	s_twitter_icon_layer = bitmap_layer_create(GRect(128, 152, 14, 14));
+	bitmap_layer_set_bitmap(s_twitter_icon_layer, s_twitter_icon_bitmap);
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_twitter_icon_layer));
+	
+	// Create tweet Layer
+	s_tweet_layer = text_layer_create(GRect(2, 124, 140, 36));
+	text_layer_set_background_color(s_tweet_layer, GColorClear);
+	text_layer_set_text_color(s_tweet_layer, GColorDukeBlue);
+	text_layer_set_text_alignment(s_tweet_layer, GTextAlignmentLeft);
+	text_layer_set_text(s_tweet_layer, "Loading...");
+	
+	// Create GFont
+	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BAKSHEESH_44));
+	
+	// Improve the layout to be more like a watchface
+	text_layer_set_font(s_time_layer, s_time_font);
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	
+	// Add it as a child layer to the Window's root layer
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+	
+	// Create second custom font, apply it and add to Window
+	s_tweet_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BAKSHEESH_11));
+	text_layer_set_font(s_tweet_layer, s_tweet_font);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_tweet_layer));
+	
+	// Register with TickTimerService
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+	
+}
+
+static void main_window_unload(Window * window) {
+	// Destroy TextLayer
+	text_layer_destroy(s_time_layer);
+	
+	// Destroy GBitmap
+	gbitmap_destroy(s_background_bitmap);
+	gbitmap_destroy(s_twitter_icon_bitmap);
+	
+	// Destroy BitmapLayer
+	bitmap_layer_destroy(s_background_layer);
+	bitmap_layer_destroy(s_twitter_icon_layer);
+	
+	// Unload GFont
+	fonts_unload_custom_font(s_time_font);
+	
+	// Destroy tweet elements
+	text_layer_destroy(s_tweet_layer);
+	fonts_unload_custom_font(s_tweet_font);
+}
+
+static void inbox_received_callback(DictionaryIterator * iterator, void * context) {
+	// Store incoming information
+	static char tweet_buffer[80];
+	
+	// Read first item
+	Tuple * t = dict_read_first(iterator);
+	
+	// For all items
+	while(t != NULL) {
+		// Which key was received?
+		switch(t->key) {
+			case KEY_TWEET:
+				snprintf(tweet_buffer, sizeof(tweet_buffer), "%.76s...", t->value->cstring);
+				break;
+			default:
+				APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+				break;
+		}
+		
+		// Look for next item
+		t = dict_read_next(iterator);
+	}
+	
+	// Assemble full string and display
+	text_layer_set_text(s_tweet_layer, tweet_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+	APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void init(void) {
+	// Create main Window element and assign to pointer
+	s_main_window = window_create();
+	
+	// Set handlers to manage the elements inside the Window
+	window_set_window_handlers(s_main_window, (WindowHandlers) {
+		.load = main_window_load,
+		.unload = main_window_unload
+	});
+	
+	// Show the Window on the watch, with animated=true
+	window_stack_push(s_main_window, true);
+	
+	// Make sure the time is displayed from the start
+	update_time();
+	
+	// Register callbacks
+	app_message_register_inbox_received(inbox_received_callback);
+	app_message_register_inbox_dropped(inbox_dropped_callback);
+	app_message_register_outbox_failed(outbox_failed_callback);
+	app_message_register_outbox_sent(outbox_sent_callback);
+	
+	// Open AppMessage
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+static void deinit(void) {
+	// Destroy Window
+	window_destroy(s_main_window);
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
---
+++ src/js/main.js
@@ -0,0 +1,73 @@
+// CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET defined in secrets.js
+
+function getTweets() {
+	// Construct URL
+	var url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+	console.log('URL ' + url);
+	
+	// OAuth signature
+	var nonce = Date.now(), timestamp = Math.floor(Date.now() / 1000);
+	var base_signature = 'GET&' + encodeURIComponent(url) + 
+		'&count%3D1' +
+		'%26oauth_consumer_key%3D' +  encodeURIComponent(CONSUMER_KEY) +
+		'%26oauth_nonce%3D'+ nonce +
+		'%26oauth_signature_method%3DHMAC-SHA1' +
+		'%26oauth_timestamp%3D' + timestamp +
+		'%26oauth_token%3D' + encodeURIComponent(ACCESS_TOKEN) +
+		'%26oauth_version%3D1.0' +
+		'%26screen_name%3DHalo';
+	var signing_secret = encodeURIComponent(CONSUMER_SECRET) + '&' + encodeURIComponent(ACCESS_SECRET);
+	var signature = CryptoJS.HmacSHA1(base_signature, signing_secret).toString(CryptoJS.enc.Base64);
+	
+	// Send request to Twitter
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+		// responseText contains a JSON object with weather info
+		var json = JSON.parse(this.responseText);
+		console.log('Got tweet: ' + json[0].text);
+			
+		// Assemble dictionary using our keys
+		var dictionary = { 'KEY_TWEET': json[0].text };
+			
+		// Send to Pebble
+		Pebble.sendAppMessage(dictionary,
+			function(e) {
+				onsole.log('Tweet sent to Pebble successfully!');
+			},
+			function(e) {
+				console.log('Error sending tweet to Pebble!');
+			}
+		);
+	};
+	xhr.onerror = function() {
+		console.log('Error! ' + this.status + ' ' + this.statusText);
+	};
+	xhr.open('GET', url + '?screen_name=Halo&count=1');
+	xhr.setRequestHeader('Authorization',
+		'OAuth oauth_consumer_key="' + CONSUMER_KEY + '", ' +
+		'oauth_nonce="' + nonce + '", ' + 
+		'oauth_signature="' + encodeURIComponent(signature) + '", ' +
+		'oauth_signature_method="HMAC-SHA1", ' +
+		'oauth_timestamp="' + timestamp + '", ' +
+		'oauth_token="' + ACCESS_TOKEN + '", ' +
+		'oauth_version="1.0"');
+	
+	xhr.send();
+}
+
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+	function(e) {
+		console.log('PebbleKit JS ready!');
+		getTweets();
+	}
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+	function(e) {
+		console.log('AppMessage received!');
+		getTweets();
+	}                     
+);
